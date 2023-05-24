c5592ad4dbc8e6bdc59cec35b10ae6a85f0c008c
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-05-24 16:40:13 +0200

    Added sidebar items

---
+++ script.js
@@ -0,0 +1,13 @@
+const sidebarItems = Array.from(document.querySelectorAll('.sidebar-item')); 
+
+sidebarItems.forEach(item => {
+    item.addEventListener('click', () => {
+      item.classList.add('sidebar-item-clicked');
+  
+      sidebarItems.forEach(otherItem => {
+        if (otherItem !== item) {
+          otherItem.classList.remove('sidebar-item-clicked');
+        }
+      });
+    });
+  });
