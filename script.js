ed495441af7babec7367ed16490f2ff819dbf0b8
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-05-26 13:04:00 +0200

    Added icons styling on click and favicon

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sidebarItems = Array.from(document.querySelectorAll('.sidebar-item'));
 const sidebarToggle = document.getElementById('Open');
 const header = document.getElementById('header');
 const main = document.getElementById('main');
+const cardIcons = Array.from(document.querySelectorAll('.white-icon'));
 
 sidebarItems.forEach(item => {
   item.addEventListener('click', () => {
@@ -15,14 +16,27 @@ sidebarItems.forEach(item => {
   });
 });
 
+cardIcons.forEach(icon => {
+  icon.addEventListener('click', () => {
+    if (icon.classList.contains('white-icon')) {
+      icon.classList.remove('white-icon')
+      icon.classList.add('clicked-icon')
+    }
+    else {
+      icon.classList.remove('clicked-icon')
+      icon.classList.add('white-icon')
+    }
+  });
+});
+
 sidebarToggle.addEventListener('change', function () {
   if (sidebarToggle.checked) {
     document.body.style.overflow = 'hidden';
     setTimeout(() => {
       header.classList.add('blur');
-    main.classList.add('blur');
+      main.classList.add('blur');
     }, 155);
-  } 
+  }
   else {
     document.body.style.overflow = 'auto';
     header.classList.remove('blur');
